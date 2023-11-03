ddf37b43f170a8502e6b909815516f11f9ff0bee
bsx_npc <BsxHomu@outlook.com>
2023-11-03 21:04:45 +0800

    feat: metadata

--- src/app/favorites/layout.tsx
+++ src/app/favorites/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Favorites',
+    description: 'All your favorite videos are here'
+}
+
 export default function Layout({ children, modal }: { children: React.ReactNode; modal: React.ReactNode }) {
     return (
         <>
