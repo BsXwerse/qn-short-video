df8e621b070fa2998fc37731e2d1eefdca851d75
bsx_npc <BsxHomu@outlook.com>
2024-06-12 11:15:34 +0800

    fix: search params

--- src/app/(watch)/leftbar.tsx
+++ src/app/(watch)/leftbar.tsx
@@ -19,7 +19,7 @@ export default function LeftBar() {
 			) : (
 				<div className="hidden h-screen left-0 w-60 shrink-0 ml-5 border-r-[1px] border-foreground/30 lg:flex flex-col gap-2 px-8 py-14 text-foreground overflow-auto">
 					{data?.map((x) => (
-						<Link href={`/tag/${x.name}`} key={x.id}>
+						<Link href={`/?tag=${x.name}`} key={x.id}>
 							<div className=" px-5 inline-flex gap-2 h-14 rounded-full items-center  hover:bg-foreground/10 transition-colors duration-150 ">
 								<span>{x.name}</span>
 							</div>
--- src/app/(watch)/page.tsx
+++ src/app/(watch)/page.tsx
@@ -1,8 +1,12 @@
 import { getByTag } from '@/actions/video';
 import { PAGE_SIZE } from '@/common/constants';
-import Player from '@/components/player';
+import Player from '@/app/(watch)/player';
 
-export default async function Home() {
-	const preData = await getByTag(0, PAGE_SIZE);
-	return <Player preData={preData} />;
+export default async function Home({
+	searchParams,
+}: { searchParams: { [key: string]: string | string[] | undefined } }) {
+	const search = searchParams?.['tag'] ?? undefined;
+	const tag = Array.isArray(search) ? search[0] : search;
+	const preData = await getByTag(0, PAGE_SIZE, tag);
+	return <Player preData={preData} tag={tag} />;
 }
